b8ebfd4de4df24b3cf52407f56dbbf875d9c9447
wai1er <Jdane@mail.ru>
2021-03-10 15:22:17 +0300

    login check added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,17 @@
+from selenium.common.exceptions import имя_исключения
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self): 
-        self.browser.get(self.url)
+        self.browser.get(self.url)    
+    
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (имя_исключения):
+            return False
+        return True
