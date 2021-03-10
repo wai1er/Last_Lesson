28f1cba18c57c258e403c18468e25746b7f65e61
wai1er <Jdane@mail.ru>
2021-03-10 16:40:17 +0300

    finally added tests

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REG_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN = (By.CSS_SELECTOR, "#id_login-username")
+    PASS = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_REG = (By.CSS_SELECTOR, "#id_registration-email")
+    PASS_REG = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASS_REG_CHECK = (By.CSS_SELECTOR, "#id_registration-password2")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from Last_Lesson.pages.base_page import BasePage
 from Last_Lesson.pages.locators import MainPageLocators
-from selenium.webdriver.common.by import By
+from Last_Lesson.pages.login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url) 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
